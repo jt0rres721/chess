b05b8c8dac5a845bf9c41224cde6fd73778545a4
Jose Torres <jt0rres7@byu.edu>
2025-03-03 20:12:22 -0700

    Complete initial register handle and UserService call

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,14 @@
 package service;
 
+import server.RegisterResult;
+import server.RegisterRequest;
+
 public class UserService {
+    public static RegisterResult register(RegisterRequest req){
+
+
+        return new RegisterResult("testuser", "testtoken");
+    }
+
+
 }
